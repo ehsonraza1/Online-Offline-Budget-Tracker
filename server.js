cf7e7d3d33ceba9f9470c4478a1c25a1b681ee08
Ehson Raza <ehson.raza1@outlook.com>
2020-09-19 14:16:47 -0500

    server.js changes

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
@@ -15,10 +16,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+// Prod Database
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false,
 });
+
+// let uri = "mongodb://localhost/budget";
+// if (process.env.NODE_ENV === "production") {
+//   uri = process.env.MONGODB_URI;
+// }
 
 // routes
 app.use(require("./routes/api.js"));
